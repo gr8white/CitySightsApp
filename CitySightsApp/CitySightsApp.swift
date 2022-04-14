945381335d14fca7556e4e7fb9ae98bce366fb91
Derrick White <derrick.white@umbrage.com>
2022-04-13 19:27:42 -0500

    Initial Commit

---
+++ CitySightsApp/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySightsApp
+//
+//  Created by Derrick White on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LaunchView()
+        }
+    }
+}
---
+++ CitySightsApp/Views/LaunchView.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchView.swift
+//  CitySightsApp
+//
+//  Created by Derrick White on 4/13/22.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
